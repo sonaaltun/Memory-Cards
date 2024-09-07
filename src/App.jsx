f50507f3a8dc42249419b969059ea7a3e4375f5c
sonaaltun <sonaaltun21@gmail.com>
2024-09-07 16:29:55 +0300

    Memory Card Oyunu

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navi from './components/Navi'
+import Board from './components/Board'
+
+const App = () => {
+  return (
+    <div>
+      <Navi/>
+      <Board/>
+    </div>
+  )
+}
+
+export default App
